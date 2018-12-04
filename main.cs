18b4dee7563c0cf1876fd8fc45fc61c1a7d96cee
turatsinzejunior83 <45234656+turatsinzejunior83@users.noreply.github.com>
2018-12-04 13:20:17 +0300

    main on class
    another added on class

---
+++ main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double t;
+            double q;
+            string k;
+            Console.WriteLine("ENTER NUMBER A");
+              w = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("ENTER number B ");
+              Z = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("PLEASE SELECT OP");
+              K = Console.ReadLine();
+              var calculator = new calculator();
+              calculator.w = t;
+              calculator.z = q;
+
+
+            switch(p)
+            {
+                case "+":
+
+                    Console.WriteLine("output= {0}", calculator.addition(w, z));
+
+                    break;
+                case "-":
+
+                    Console.WriteLine("output= {0}", calculator.sub(w, z));
+
+                    break;
+                case "*":
+
+                    Console.WriteLine("output= {0}", calculator.multiplication(w, z));
+
+                    break;
+
+                case "/":
+
+                    Console.WriteLine("output= {0}", calculator.devision(w, z));
+
+                    break;
+                default:
+                    Console.WriteLine("invalid op");
+                    break;
+            }
+
+              Console.ReadLine();
+        }
+    }
+}
