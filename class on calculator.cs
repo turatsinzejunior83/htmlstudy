ef96022fdacec2f65dbb63c8b259c8d50811c0ca
turatsinzejunior83 <45234656+turatsinzejunior83@users.noreply.github.com>
2018-12-04 13:19:18 +0300

    class on calculator
    added

---
+++ class on calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    class calculator
+    {
+        static void main(string[] args)
+        {
+        public double w,z;
+        
+
+        public double addition(double w, double z)
+        {
+            return (w + z);
+        }
+
+        public double sub(double w, double z)
+        {
+            return (w - z);
+        }
+        public double multiplication(double w, double z)
+        {
+            return (w * z);
+        }
+        public double devision(double w, double z)
+        {
+            if (z == 0)
+                Console.WriteLine("invalid operation");
+
+            return (w / z);
+
+        }
+
+    }  
+            
+    
+}
